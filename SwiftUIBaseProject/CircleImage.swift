978c5c0bc5005ae752f5631dff5512e6f5d24920
Iren <irene19098@gmail.com>
2023-07-15 19:51:37 +0300

    lesson-1: Landmark. Step 1

---
+++ SwiftUIBaseProject/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftUIBaseProject
+//
+//  Created by Ирина Кольчугина on 15.07.2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4.0)
+            }
+            .shadow(radius: 7.0)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
