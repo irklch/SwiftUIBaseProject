3a676c6d5ac02702675db51d77157a2eb5eff5d3
Iren <irene19098@gmail.com>
2023-07-15 19:10:12 +0300

    main commit

---
+++ SwiftUIBaseProject/SwiftUIBaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseProjectApp.swift
+//  SwiftUIBaseProject
+//
+//  Created by Ирина Кольчугина on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
